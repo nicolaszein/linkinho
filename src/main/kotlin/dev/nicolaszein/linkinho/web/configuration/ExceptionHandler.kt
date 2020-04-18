f427807f3d71f24c0a8aad191314b3b0b2c440aa
Nicolas Zein <nicolas.zein@creditas.com.br>
2020-04-18 11:52:35 -0300

    Add error handler

---
+++ src/main/kotlin/dev/nicolaszein/linkinho/web/configuration/ExceptionHandler.kt
@@ -0,0 +1,74 @@
+package dev.nicolaszein.linkinho.web.configuration
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+data class ExceptionResponse(
+    val code: String,
+    val message: String
+)
+
+data class DetailExceptionResponse(
+    val code: String,
+    val message: String,
+    val details: MutableList<ErrorDetail> = mutableListOf()
+) {
+    fun addErrorDetails(target: String, message: String) {
+        this.details.add(ErrorDetail(target, message))
+    }
+}
+
+data class ErrorDetail(
+    val target: String,
+    val message: String
+)
+
+@ControllerAdvice
+class ExceptionHandler {
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun invalidJsonInput(
+        ex: HttpMessageNotReadableException
+    ): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            code = "INPUT_VALIDATION_ERROR",
+            message = "Invalid Json input"
+        )
+
+        return ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException::class)
+    fun invalidInputFormat(): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            code = "UNSUPPORTED_MEDIA_TYPE",
+            message = "Input format is unsupported"
+        )
+
+        return ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun invalidInput(
+        ex: MethodArgumentNotValidException
+    ): ResponseEntity<DetailExceptionResponse> {
+        val exceptionResponse = DetailExceptionResponse(
+            code = "INPUT_VALIDATION_ERROR",
+            message = "Some fields are not valid"
+        )
+
+        for (error in ex.bindingResult.fieldErrors) {
+            exceptionResponse.addErrorDetails(
+                target = error.field,
+                message = error.defaultMessage
+            )
+        }
+
+        return ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+}
+
